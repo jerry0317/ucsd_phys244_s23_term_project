9315e23fc847e93d511d427c6e48154ced4a58e2
Jerry Yan <jerry0317@users.noreply.github.com>
2023-05-10 22:49:45 -0700

    test for cosmo_box_gravity_only_3d

--- bash_run_logs/05102023_1.sh
+++ bash_run_logs/05102023_1.sh
@@ -2,4 +2,13 @@
 
 srun --partition=shared --pty --account=csd759 --nodes=1 --ntasks-per-node=1 --cpus-per-task=16 --mem=64G -t 04:00:00 --wait=0 --export=ALL /bin/bash
 
+# export SYSTYPE="Ubuntu"
+
+module purge
 module load slurm cpu/0.15.4 gcc openmpi gsl gmp fftw hdf5 hwloc py-pip
+
+make -j 16
+
+pip install h5py matplotlib
+
+bash test_cosmo_box_gravity_only_3d.sh | tee -a ../out_file_logs/05102023_1.out
