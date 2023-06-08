b128c7da058043e273002149b8f09d6987bd59bc
Jerry Yan <jerry0317@users.noreply.github.com>
2023-06-08 10:53:16 -0700

    Update test_cosmo_box_gravity_only_3d_PMGRID_MemSize.sh

--- arepo/test_cosmo_box_gravity_only_3d_PMGRID_MemSize.sh
+++ arepo/test_cosmo_box_gravity_only_3d_PMGRID_MemSize.sh
@@ -30,7 +30,7 @@ exit $return_value
 fi
   
 ## change to RUNDIR in subshell and execute test simulation
-(cd ${RUNDIR} && mpiexec -np ${NUMBER_OF_TASKS} --oversubscribe ./Arepo ./param.txt)
+(cd ${RUNDIR} && mpiexec AMDuProfCLI-bin collect --config tbp -o amduprof_out_"%Y-%m-%dT%H%M%S%:z" -np ${NUMBER_OF_TASKS} --oversubscribe ./Arepo ./param.txt)
 ((return_value=$?))    ## get return value
 if [ $return_value != 0 ]    ## check return value
 then echo "ERROR: test.sh:\t" $DIR "\t execution failed!"
